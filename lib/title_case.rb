9b2e1846fa24cf4321928851933208d189c67665
Ellen Krause Lehman <student@epicodus.com>
2014-12-10 11:47:34 -0800

    capitalize multiple words in a string

--- lib/title_case.rb
+++ lib/title_case.rb
@@ -1,12 +1,17 @@
 def title_case(string)
   title = []
-  title_array = string.split("")
-  title_array.each do |letter|
-    if letter == title_array.at(0)
-      title.push(letter.upcase!())
-    else
-      title.push(letter)
+  title_array = string.split()
+  title_array.each do |word|
+    each_word = []
+    word_array = word.split("")
+    word_array.each do |letter|
+      if letter == word_array.at(0)
+        each_word.push(letter.upcase!())
+      else
+        each_word.push(letter)
+      end
     end
+    title.push(each_word.join())
   end
-  title.join()
+  title.join(" ")
 end
--- spec/title_case_spec.rb
+++ spec/title_case_spec.rb
@@ -5,4 +5,9 @@ describe('title_case') do
   it("capitalizes the first letter of a word") do
     expect(title_case("cat")).to eq("Cat")
   end
+
+  it("capitalizes the first letter all words in a multiple word title") do
+    expect(title_case("the color purple")).to eq("The Color Purple")
+  end
+
 end
